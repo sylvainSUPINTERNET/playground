82b387c90175ff9702156c36a133dfe45150dfb3
sylvainSUPINTERNET <sylvain.joly00@orange.fr>
2021-09-14 00:30:42 +0200

    Update singlyLinkedList.java
    Add parent for remove operation o(1)

--- java/singlyLinkedList.java
+++ java/singlyLinkedList.java
@@ -3,9 +3,14 @@ public class Main {
 class Node {
          String data;
          Node next = null;
+         Node parent = null;
 
          public Node(String data) {
             this.data = data;
+         }
+         
+         public Node parent() {
+            return this.parent;
          }
 
          public Node next() {
@@ -29,19 +34,40 @@ class Node {
          public int getCount() {
             return this.count;
          }
+         
+         public void removeNode() {
+            this.count = count--;
+            if ( count <= 0 ) {
+                this.count = 0;
+                this.head = null;
+                this.tail = null;
+            } else if ( count == 1 ) {
+                this.head.next = null;
+                this.tail = null;
+            } else {
+                Node last = this.tail;
+                Node newLast = last.parent;
+                newLast.next = null;
+                this.tail = newLast;
+            }
+         }
 
          public Node addNode(String data){
             Node node = new Node(data);
             this.count++;
 
             if ( this.head == null ) {
+                node.parent = null;
                this.head = node;
             } else {
                if ( count == 2 ) {
+                   node.parent = this.head;
+                   
                   this.head.next = node;
                   this.tail = node;
                } else {
                   Node old = this.tail;
+                  node.parent = old;
                   old.next = node;
                   this.tail = node;
                }
@@ -58,9 +84,13 @@ class Node {
       ld.addNode("Im tail");
       System.out.println(ld.head.data);
       System.out.println(ld.head.next.data);
+      //System.out.println(ld.head.next.parent.data); // display im head 
       System.out.println(ld.head.next.next.data);
       System.out.println(ld.head.next.next.next); // null ofc
       System.out.println(ld.getCount());
+      
+/*      ld.removeNode();
+      System.out.println(ld.tail.data); // im body */
     }
 }
 
